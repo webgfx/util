563f0bb21180b6c3afcb41a71a22a26c1f4795ac
Yang Gu <yang.gu@intel.com>
2021-06-11 10:33:45 +0800

    Add Tint dir

--- base.py
+++ base.py
@@ -1205,6 +1205,7 @@ class Util:
     PROJECT_MESA_BACKUP_DIR =  format_slash.__func__('%s/backup' % PROJECT_MESA_DIR)
     PROJECT_SKIA_DIR =  format_slash.__func__('%s/skia' % PROJECT_DIR)
     PROJECT_TFJS_DIR =  format_slash.__func__('%s/tfjs' % PROJECT_DIR)
+    PROJECT_TINT_DIR =  format_slash.__func__('%s/tint' % PROJECT_DIR)
     PROJECT_TOOLKIT_DIR =  format_slash.__func__('%s/toolkit' % PROJECT_DIR)
     PROJECT_V8_DIR =  format_slash.__func__('%s/v8' % PROJECT_DIR)
     PROJECT_WASM_DIR =  format_slash.__func__('%s/wasm' % PROJECT_DIR)
