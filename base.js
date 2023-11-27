66b93264cbe801bdfae5ec30336a63181dc16e55
Yang Gu <yang.gu@intel.com>
2023-11-27 23:36:21 +0800

    base.js: Add more functions

--- base.js
+++ base.js
@@ -1,10 +1,5 @@
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+function getFloat(value, decimalPlaces = 2) {
+  return parseFloat(value.toFixed(decimalPlaces));
 }
 
 function getParamByName(name, url) {
@@ -20,6 +15,19 @@ function getParamByName(name, url) {
   return decodeURIComponent(match[2].replace(/\+/g, ' '));
 }
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function randomInt(max, min = 0) { return Math.round(Math.random() * (max - min)) + min; }
+function randomFloat(max, min = 0) { return Math.random() * (max - min) + min; }
+function randomElement(arr) { return arr[randomInt(arr.length - 1)]; }
+
 function readFile(file, callback) {
   let xhr = new XMLHttpRequest();
   xhr.overrideMimeType('application/json');
@@ -30,4 +38,25 @@ function readFile(file, callback) {
     }
   };
   xhr.send();
+}
+
+async function readFileAsync(file) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("HEAD", file);
+  //xhr.send();
+
+  return new Promise(function (resolve, reject) {
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4) {
+        if (xhr.status >= 300) {
+          reject("Error, status code = " + xhr.status);
+        } else {
+          resolve(xhr.responseText);
+        }
+      }
+    };
+    xhr.overrideMimeType("application/json");
+    xhr.open("get", file, true);
+    xhr.send();
+  });
 }
